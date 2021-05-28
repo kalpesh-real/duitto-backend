5313e3b72bf81780e850dd87ad7006d3e0bef47c
kalpesh-real <61679981+kalpesh-real@users.noreply.github.com>
2021-05-28 19:24:00 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './component/header-component/header-component.component';
+import { UserLoginRegistrationComponent } from './pages/user-login-registration/user-login-registration.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { OrderPageComponent } from './pages/order-page/order-page.component';
+import { Dialog } from './Utils/dialog';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './component/footer/footer.component';
+import { InterceptorService } from './services/interceptor.service';
+import { ConfirmEmailComponent } from './pages/confirm-email/confirm-email.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { UserAdminPanelComponent } from './pages/user-admin-panel/user-admin-panel.component';
+import {MatTableModule} from '@angular/material/table';
+import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
+import { AdminUpdateOrderComponent } from './pages/admin-update-order/admin-update-order.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponentComponent,
+    UserLoginRegistrationComponent,
+    HomePageComponent,
+    OrderPageComponent,
+    FooterComponent,
+    ConfirmEmailComponent,
+    ResetPasswordComponent,
+    UserAdminPanelComponent,
+    AdminPanelComponent,
+    AdminUpdateOrderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatStepperModule,
+    MatIconModule,
+    MatMenuModule,
+    NgbModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+    
+  ],
+  providers: [Dialog,{ provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true },MatDatepickerModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
